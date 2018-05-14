af10464e1a40fbe23ec82bb506d461783c40eb79
Matt Land <mland@sparefoot.com>
2018-05-14 10:45:22 -0400

    stuffs

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='fakesubdepmodule',
+    py_modules=[''],
+    version='0.0.0',
+    description='helps test pip',
+    url='git@github.com:matt-land/fake_subdep_module.git',
+    author='No One',
+    author_email='noone@nowhere.com',
+)
